015ce0c6ff2b1046beea1420a09c5889a86f1175
GreenBlackSky <greenblacksky@gmail.com>
2021-11-04 19:48:59 +0300

    NL added

--- DiscordCountBot.py
+++ DiscordCountBot.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 
 from discord.ext import commands
-
+from num2words import num2words
 
 logger = logging.getLogger('discord')
 discordCountBot = commands.Bot(command_prefix='!')
@@ -28,7 +28,8 @@ async def count(ctx: commands.context.Context, *args):
 
     for i in range(int(countdown)):
         await asyncio.sleep(1)
-        await ctx.send(i)
-        logger.info(f"counting to {countdown} for {ctx.author}: {i}")
+        message = num2words(i + 1)
+        await ctx.send(message)
+        logger.info(f"counting to {countdown} for {ctx.author}: {message}")
 
     logger.info(f"countdoun to {countdown} for {ctx.author} finished")
